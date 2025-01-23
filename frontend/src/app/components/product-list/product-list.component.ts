ab07605088ef4ca708925951f07a1da41c494cdc
Angel Zlatenov <angel.zlatenov@westernacher.com>
2025-01-23 21:57:51 +0200

    feat: Display category name in Product list grid

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Product} from "../../common/product";
-import {ProductService} from "../../services/product.service";
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Product } from "../../common/product";
+import { ProductService } from "../../services/product.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -11,6 +11,7 @@ import {ActivatedRoute} from "@angular/router";
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
+  currentCategoryName: string = 'Default Category';
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -21,18 +22,24 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  listProducts() {
+  listProducts(): void {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    const hasCategoryName: boolean = this.route.snapshot.paramMap.has('name');
 
     if (hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     } else {
       this.currentCategoryId = 1;
     }
+
+    if (hasCategoryName) {
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    }
+
     this.productService.getProductList(this.currentCategoryId).subscribe(
         data => {
           this.products = data;
         }
-    )
+    );
   }
 }
